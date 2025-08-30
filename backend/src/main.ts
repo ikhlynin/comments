1198903a9cbae95f7a17b79c63e12c66c00b5c10
Ihor Khlynin <ihorkhlynin@gmail.com>
2025-08-30 20:30:41 +0300

    api port fixed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,7 @@ async function bootstrap() {
   console.log('DB_URL:', process.env.DB_URL);
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:
-      process.env.FRONTEND_URL ||
-      'https://frontend-production-0da9.up.railway.app',
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
