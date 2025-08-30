1eebadc0e39836ba46f4ab737c75740ebf837147
Ihor Khlynin <ihorkhlynin@gmail.com>
2025-08-30 20:39:03 +0300

    api port fixed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('DB_URL:', process.env.DB_URL);
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
