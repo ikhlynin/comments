c61dc12a8b18b62c8f20018128871b2256b5ed41
Ihor Khlynin <ihorkhlynin@gmail.com>
2025-08-30 18:34:27 +0300

    api url fixed

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     process.env.REACT_APP_API_URL ||
-    "https://backend-production-7295.up.railway.app:5000",
+    "https://backend-production-7295.up.railway.app",
   withCredentials: true,
 });
 
