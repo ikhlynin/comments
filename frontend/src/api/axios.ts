6e8470be4a11831c5366535e39b6e13e2325a504
Ihor Khlynin <ihorkhlynin@gmail.com>
2025-09-01 03:17:07 +0300

    fixes for frontend env

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://backend-production-7295.up.railway.app",
   withCredentials: true,
 });
 
