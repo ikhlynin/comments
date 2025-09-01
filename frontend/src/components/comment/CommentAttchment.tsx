f9b9b51ca83ed78624444e3682232ef1dd2a71d6
Ihor Khlynin <ihorkhlynin@gmail.com>
2025-09-01 04:52:42 +0300

    text selection fixed

--- frontend/src/components/comment/CommentAttchment.tsx
+++ frontend/src/components/comment/CommentAttchment.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import { IMAGE_TYPES } from "../../types/types";
 
 const CommentAttachment: React.FC<{ path: string }> = ({ path }) => {
-  const fullPath = `${process.env.REACT_APP_API_URL}${path}`;
+  const rawPath =
+    process.env.REACT_APP_API_URL ||
+    "https://backend-production-7295.up.railway.app";
+  const fullPath = `${rawPath}${path}`;
   const isImage = IMAGE_TYPES.test(path);
   return isImage ? (
     <img src={fullPath} alt="attachment" className="comment__attachment__img" />
